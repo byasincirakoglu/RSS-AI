0e674bbe034656b695d131fb89e4d20bfb793f66
Berat Çırakoğlu <beratcirakoglu@dinamikbt.com>
2023-08-24 10:54:24 +0300

    Helper düzenlendir.

--- Dinamik Oto Etiket/Helper/Helper.cs
+++ Dinamik Oto Etiket/Helper/Helper.cs
@@ -13,10 +13,7 @@ namespace Dinamik_Oto_Etiket.Helper
     {
         public static void InitialData(DataGridView view) 
         {
-
-
             view.DataSource = DbConnection.RunSql("SELECT CARI_ISIM,CARI_ILCE,CARI_KOD,CARI_TEL,CARI_IL,CARI_ADRES,S_YEDEK2 FROM TBLCASABIT WHERE CARI_KOD LIKE 'ZZD%'");
-
         }
     }
 }
